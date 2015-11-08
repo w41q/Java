061fce85fca292ba2f9584897499490cbd19b50f
w41q <alexwang041@gmail.com>
2015-11-07 17:05:08 -0800

    First Junit Test

---
+++ Junit/CalculatorTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class CalculatorTest {
+
+	@Test
+	public void evaluatesExpression() {
+		//fail("Not yet implemented");
+		Calculator calculator = new Calculator();
+	    int sum = calculator.evaluate("1+2+3");
+	    assertEquals(6, sum);
+	}
+
+}
